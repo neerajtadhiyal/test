aae061f0e0a512763d8c88ee11ef1974da7dbf23
neeraj <ntadhiyal@gmail.com>
2019-10-30 12:31:40 +0530

    they r being commited

--- first.cpp
+++ first.cpp
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
-{
+{ 
+	printf("bla bla bla");
  pritf("hello it will interprite the number 10 time so get redy");
  printf("\n");
- for (i=0;i<=10;i++)
+ for (int i=0;i<=10;i++)
  {
    printf("%d \t",i);
    
