285a0d6888e8905c2da074a6f71545a969e20fd9
neerajtadhiyal <57168201+neerajtadhiyal@users.noreply.github.com>
2019-10-30 12:26:57 +0530

    this was added from git

--- first.cpp
+++ first.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
- pritf("hello it will interprite the number 10 time so get redy");
+ printf("hello it will interprite the number 10 time so get redy");
  printf("\n");
  for (i=0;i<=10;i++)
  {
